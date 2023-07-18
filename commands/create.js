e4fe07c753e5d7206b96d076708a1263ef0d86e2
Matt Wiggins <matthew@wiggins.dev>
2023-07-17 19:34:56 -0500

    update packages and fix error handling

--- commands/create.js
+++ commands/create.js
@@ -9,47 +9,52 @@ module.exports = {
 	description: 'Creates new game account.',
   DMonly: true,
 	execute(message, args) {
-        if(!args[0]) return message.reply(`You need to add a username after the command. \nUsage: **!create <username> <password>**`)
-        if(!args[1]) return message.reply(`You need to add a password after the username. \nUsage: **!create <username> <password>**`)
-        let username = args[0];
-        let password = args[1];
-        connection.query('USE acore_auth')
-          connection.query('select COUNT(username) from account where reg_mail = ?', [message.author.id], (error, results, fields) => {
 
-            if (error) return message.reply('An error occured.')
+    try {
+      if(!args[0]) return message.reply(`You need to add a username after the command. \nUsage: **!create <username> <password>**`)
+      if(!args[1]) return message.reply(`You need to add a password after the username. \nUsage: **!create <username> <password>**`)
+      let username = args[0];
+      let password = args[1];
+      connection.query('USE acore_auth')
+        connection.query('select COUNT(username) from account where reg_mail = ?', [message.author.id], (error, results, fields) => {
 
-
-            if (Object.values(results[0])[0] <= 25) {
-              try {
-              soap.Soap(`account create ${username} ${password}`)
-              .then(result => { 
-
-                console.log(result)
-                if(result.faultString) return message.reply("Username already exists.") 
-              
-                else connection.query(`UPDATE account set reg_mail = '${message.author.id}' WHERE username = '${username}'`)
+          if (error) return message.reply('An error occured.')
 
 
-                    const embed = new Discord.MessageEmbed()
-                    .setColor(config.color)
-                    .setTitle('Account Created')
-                    .setDescription('Take a look at your account info below:')
-                    .addField('Username', username, true)
-                    .addField('Password', password, true)
-                    .setTimestamp()
-                    .setFooter('Create command', client.user.displayAvatarURL());
+          if (Object.values(results[0])[0] <= 25) {
+            try {
+            soap.Soap(`account create ${username} ${password}`)
+            .then(result => { 
+
+              console.log(result)
+              if(result.faultString) return message.reply("Username already exists.") 
             
-                  message.channel.send(embed);
+              else connection.query(`UPDATE account set reg_mail = '${message.author.id}' WHERE username = '${username}'`)
 
-                })
-              } catch (error) {
-                console.log(error)
-              }
-            } else {
-              message.reply('You already have 25 accounts!')
-            }
-            
-          })
+
+                  const embed = new Discord.MessageEmbed()
+                  .setColor(config.color)
+                  .setTitle('Account Created')
+                  .setDescription('Take a look at your account info below:')
+                  .addField('Username', username, true)
+                  .addField('Password', password, true)
+                  .setTimestamp()
+                  .setFooter('Create command', client.user.displayAvatarURL());
           
+                message.channel.send(embed);
+
+              })
+            } catch (error) {
+              console.log(error)
+            }
+          } else {
+            message.reply('You already have 25 accounts!')
+          }
+          
+        })
+    } catch (error) {
+      console.log(error)
+      message.reply('Account creation failed due to an error.')
+    }     
 	},
 };
---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+        token: "MTA2MTExNzk4MDExODg4NDUxNA.Gz3h6Z.gTYXKVzg-uYeJf291I42-cImEUJkxr-qmNp2Jw",
+        prefix: "!",
+        statusMessage: "with my 25 characters!",
+        color: "#0000FF",
+
+        databaseHost: 'localhost',
+        databaseUser     : 'user',
+        databasePassword : 'password',
+
+        soapPort: 7878,
+        soapHostname: "127.0.0.1",
+        soapAuth: "username:passwd"
+
+}
