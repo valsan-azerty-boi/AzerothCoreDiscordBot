d455191bef7cad5b6e543b6bcde3bf2d7286fa05
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-04-02 16:27:20 +0200

    fix online

--- commands/online.js
+++ commands/online.js
@@ -14,23 +14,20 @@ const connection = mysql.createPool({
 
 module.exports = {
     name: "online",
-    description: "Gives list of online players.",
+    description: "Gives total count of online players.",
     DMonly: false,
     async execute(message, args) {
         try {
             await connection.query("USE " + config.databaseCharacter);
 
-            const [results1] = await connection.query("SELECT name FROM characters WHERE online = 1");
-
-            let onlinePlayers = results1.map(row => row.name);
-            let counter = onlinePlayers.length;
-            let description = counter > 0 ? onlinePlayers.join(", ") : "There is no one online.";
+            const [countResults] = await connection.query("SELECT COUNT(name) AS onlineCount FROM characters WHERE online = 1");
+            const onlineCount = countResults[0].onlineCount;
 
             const embed = new EmbedBuilder()
                 .setColor(config.color)
                 .setTitle("Online Players")
-                .setDescription(description)
-                .addFields({ name: "Amount of characters online:", value: `${counter} characters` }) // Fixed template literal
+                .setDescription("Gives total count of online players")
+                .addFields({ name: "Amount of characters online:", value: `${onlineCount}` })
                 .setTimestamp()
                 .setFooter({ text: "Online command", iconURL: client.user?.displayAvatarURL() || "" });
 
